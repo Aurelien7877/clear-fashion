eb083e0c97b02eae6a53807ba354e9a361efe7be
Aurelien Pouxviel <aurelien.pouxviel@edu.devinci.fr>
2023-02-14 15:18:39 +0100

    Scrap de montlimard

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimartbrand = require('./eshops/montlimartbrand');
+//montlimart = https://www.montlimart.com/99-vetements
+//dedicated = https://www.dedicatedbrand.com/en/men/news
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.montlimart.com/99-vetements') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await montlimartbrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
