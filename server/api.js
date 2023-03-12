a88bb1c11afc5b34ca19a36e2f0c9960377799f7
Aurelien Pouxviel <aurelien.pouxviel@edu.devinci.fr>
2023-03-12 17:01:25 +0100

    feat(get-product): get a specific product

--- server/api.js
+++ server/api.js
@@ -25,7 +25,11 @@ let ObjectId = require('mongodb').ObjectId; //Obligé pour retouver l'id dans mo
 
 //Endpoint 1 : Fetch a specific product
 
-//Ok, exemple http://localhost:8092/products/640a251e422a62b7b213e39b
+//Ok, exemple http://localhost:8092/products/640df194906481cb43eb2725 (conflits donc utliser V2)
+
+// --------------- V2 ----------------- 
+// Marche parfaitement avec : http://localhost:8092/products/byID/640df194906481cb43eb2725
+
 
 /*app.get('/products/:id', async (request, response) => {
   const MONGODB_URI = 'mongodb+srv://admin:admin@clear-fashion.ykj5vxa.mongodb.net/?retryWrites=true&w=majority';
@@ -35,14 +39,32 @@ let ObjectId = require('mongodb').ObjectId; //Obligé pour retouver l'id dans mo
 	  const db =  client.db(MONGODB_DB_NAME);
 	  const collection = db.collection('products');
 	  const findProduct = request.params.id;
-	  let endpointResult = await collection.findOne({_id: ObjectId(findProduct)});
-	  response.send({result : endpointResult});
+	  let endpointResult1 = await collection.findOne({_id: ObjectId(findProduct)});
+	  response.send({result : endpointResult1});
   } catch(e){response.send({error : "id not valid !"});  }
 });*/
+
+///--------------------- V2 --------------
+
+//V2 car sinon conflits entre les 2 paramètres. Le premier endpoints products/:id croit que products/search
+// fait parti de l'id aussi. j'ai donc rajouté products/byID/:id pour vraimen les dissocier
+app.get('/products/byID/:id', async (request, response) => {
+  const MONGODB_URI = 'mongodb+srv://admin:admin@clear-fashion.ykj5vxa.mongodb.net/?retryWrites=true&w=majority';
+	const MONGODB_DB_NAME = 'clear-fashion';
+  try{
+	  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+	  const db =  client.db(MONGODB_DB_NAME);
+	  const collection = db.collection('products');
+	  const findProduct = request.params.id;
+	  let endpointResult1 = await collection.findOne({_id: ObjectId(findProduct)});
+	  response.send({result : endpointResult1});
+  } catch(e){response.send({error : "id not valid !"});  }
+});
 
 
 //Endpoint 2 : Search for specific products
 //OK, example : http://localhost:8092/products/search?limit=10&brandName=Circle&price=60
+
 app.get('/products/search', async (request, response) => {
   const MONGODB_URI = 'mongodb+srv://admin:admin@clear-fashion.ykj5vxa.mongodb.net/?retryWrites=true&w=majority';
 	const MONGODB_DB_NAME = 'clear-fashion';
@@ -64,9 +86,13 @@ app.get('/products/search', async (request, response) => {
       query.price = { $lte: parseInt(priceSearch) };
     }
 
-    let endpointResult = await collection.find(query).limit(parseInt(limitSearch)).toArray();
+    let endpointResult2 = await collection
+    .find(query)
+    .limit(parseInt(limitSearch))
+    .sort({ price: 1 })
+    .toArray();
 
-	  response.send({result : endpointResult});
+	  response.send({result : endpointResult2});
   } catch(e){response.send({error : "search not valid !"});  }
 });
 
